372b0cf82b297e0d095e2a37fe8c567874f16bb0
Preston Neal <prestonneal@Prestons-MBP.localdomain>
2018-08-15 12:00:41 -0500

    more updates

--- routes/speakers.js
+++ routes/speakers.js
@@ -4,41 +4,14 @@ let router = express.Router();
 
 router.get('/speakers', (req, res) => {
 
-    let dataFile = req.app.get("appData");
-    let myHTML;
+    let data = req.app.get("appData");
 
-    res.render('index', {
+    res.render('speakers', {
         pageTitle: "Speakers",
-        pageID: 'Home',
+        pageID: 'Speakers',
         speakers: data.speakers
 })
 
-    dataFile.speakers.forEach(function(item){
-        myHTML += `
-        <li>
-            <h2>${item.name}</h2>
-            <img src="/images/speakers/${item.shortname}.jpg" alt="">
-            <p>${item.summary}</p>
-        </li>
-        `; 
-
-    })
-    res.send(`
-    <h1>Speakers</h1>
-    <ul>
-        ${myHTML}
-    </ul>
-    `)
-
 })
-
-
-router.get('/speakers/:speakerID', (req, res) => {
-
-    let dataFile = req.app.get("appData");
-    let speaker = dataFile.speakers[req.params.speakerID];
-
-})
-
 module.exports = router;
 
